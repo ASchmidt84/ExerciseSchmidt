455175530a2956fd53896adafab319b2d5f6e1d1
andre <a.schmidt@scalamat.de>
2025-01-17 08:45:17 +0100

    Entry Generator

--- backend-service/src/main/scala/backend/projection/BlogEntryProjectionHandler.scala
+++ backend-service/src/main/scala/backend/projection/BlogEntryProjectionHandler.scala
@@ -5,47 +5,21 @@ import akka.actor.typed.ActorSystem
 import akka.persistence.query.typed.EventEnvelope
 import akka.projection.r2dbc.scaladsl.{R2dbcHandler, R2dbcSession}
 import backend.entity.BlogEntry
-import akka.cluster.sharding.typed.scaladsl.{ClusterSharding, EntityRef, EntityTypeKey}
+import akka.cluster.sharding.typed.scaladsl.EntityRef
 import akka.grpc.GrpcServiceException
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest}
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
-import akka.util.Timeout
 import io.grpc.Status
 import org.slf4j.LoggerFactory
 import play.api.libs.json.Json
-import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future, TimeoutException}
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future, TimeoutException}
 
 class BlogEntryProjectionHandler(slice: String,
-                                 implicit val system: ActorSystem[_],
-                                 targetUrl: String) extends R2dbcHandler[EventEnvelope[BlogEntry.Event]] {
+                                 protected implicit val system: ActorSystem[_],
+                                 targetUrl: String) extends R2dbcHandler[EventEnvelope[BlogEntry.Event]] with EntityAccess {
   private val logger = LoggerFactory.getLogger("BlogEntry.ProjectionHandler")
-
-
-  implicit private val ec: ExecutionContextExecutor =  system.executionContext
-  private implicit val timeout: Timeout = Timeout(30.seconds)
-  private val sharding = ClusterSharding(system)
-  private val entityKey: EntityTypeKey[BlogEntry.Command] = BlogEntry.EntityKey
-
-  private def convertError[T](response: Future[T])(implicit ec: ExecutionContext): Future[T] = {
-    response.recoverWith {
-      case _: TimeoutException =>
-        Future.failed(
-          new GrpcServiceException(Status.UNAVAILABLE.withDescription("Operation timed out"))
-        )
-      case exc =>
-        Future.failed(
-          new GrpcServiceException(Status.INVALID_ARGUMENT.withDescription(exc.getMessage))
-        )
-    }
-  }
-
-  private def entity(entityId: String): EntityRef[BlogEntry.Command] = sharding.entityRefFor(
-    entityKey,
-    entityId.trim.toLowerCase
-  )
 
   override def process(session: R2dbcSession,
                        envelope: EventEnvelope[BlogEntry.Event]): Future[Done] = envelope.event match {
